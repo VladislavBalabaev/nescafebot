92f236f7662bb0f1b83e3f0a6803dc6266b7cbb3
Vladislav Balabaev <vv.balabaev@gmail.com>
2024-02-27 14:55:49 +0300

    separate logging to console and to file

--- bot/configs/logs.py
+++ bot/configs/logs.py
@@ -3,41 +3,41 @@ import asyncio
 import logging
 from queue import Queue
 from logging import StreamHandler
-from logging.handlers import QueueHandler, QueueListener
+from logging.handlers import QueueHandler, QueueListener, RotatingFileHandler
 
 
 path = Path('logs')
+logs_path = path / "coffee.log"
 
-path.mkdir(parents=True, exist_ok=True)
-
-
-logging.basicConfig(
-    filename=path / "coffee.log",
-    encoding="utf-8",
-    # format="",
-    datefmt="%Y-%m-%d %H:%M:%S",
-)
+logging_format = logging.Formatter("%(levelname)-8s :: %(asctime)s :: %(message)s :: (%(filename)s:%(lineno)d)")
 
 
 async def init_logger():
-    logger = logging.getLogger()            # to not use root logger
-    # this logger has no handlers and it will propagate all events to be handled by root logger
-    # best: make one logger per major-subcomponent of all project
-    # debug -> info -> warning -> error -> critical
-    
+    path.mkdir(parents=True, exist_ok=True)
+
     que = Queue()
-    
-    logger.addHandler(QueueHandler(que))
-    logger.setLevel(logging.DEBUG)
-    
-    listener = QueueListener(que, StreamHandler())
+
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG)
+    root_logger.addHandler(QueueHandler(que))
+
+    console_handler = StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(fmt=logging_format)
+
+    file_handler = RotatingFileHandler(logs_path)
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(fmt=logging_format)
+
+    listener = QueueListener(que, console_handler, file_handler)          # StreamHandler - to console
+
     try:
         logging.debug(f'Logger is being started.')
         listener.start()
         logging.debug(f'Logger has been started started.')
-        
+
         while True:
             await asyncio.sleep(60)
     finally:
-        logging.debug(f'Logger is shutting down.')
+        logging.debug(f'Logger is being shutdown.')
         listener.stop()
