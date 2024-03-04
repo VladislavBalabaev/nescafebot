3447b236d395b13ae772642b6b56a9fe0c203b11
Vladislav Balabaev <vv.balabaev@gmail.com>
2024-03-04 15:56:40 +0300

    create filter for admins

--- bot/handlers/admin/admin.py
+++ bot/handlers/admin/admin.py
@@ -1,4 +1,5 @@
 import logging
+from aiogram.filters import Filter
 from aiogram.filters.command import Command
 from aiogram.filters.state import StateFilter
 from aiogram import Dispatcher, types, Router
@@ -11,13 +12,20 @@ from handlers.utils import error_sender
 router = Router()
 
 
-@router.message(StateFilter(None), Command("logs"))
+class AdminFilter(Filter):
+    def __init__(self, text: str) -> None:
+        pass
+
+    async def __call__(self, message: types.Message) -> bool:
+        return message.from_user.id in ADMINS
+
+
+@router.message(StateFilter(None), Command("logs"), AdminFilter)
 @error_sender
 async def cmd_send_logs(message: types.Message):
-    if message.from_user.id in ADMINS:
-        logging.info(f"Admin @{message.from_user.username} asked for logs.")
+    logging.info(f"Admin @{message.from_user.username} asked for logs.")
 
-        await message.answer_document(types.FSInputFile(logs_path))\
+    await message.answer_document(types.FSInputFile(logs_path))
 
 
 def register_handlers_admin(dp: Dispatcher):
