9f442f8a588d71d31c1915f1086fff95e8c1e42c
Vladislav Balabaev <vv.balabaev@gmail.com>
2024-03-06 17:26:00 +0300

    add nano

--- Dockerfile.py
+++ Dockerfile.py
@@ -2,6 +2,9 @@ FROM python:3.10.13-bullseye
 
 WORKDIR /usr/src/app
 
+RUN apt-get update
+RUN apt-get install nano
+
 COPY requirements.txt ./
 RUN pip install --no-cache-dir --upgrade pip
 RUN pip install --no-cache-dir -r requirements.txt
