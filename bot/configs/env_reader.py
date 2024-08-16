ffe79587deba438331fb1688db0d40531f883a67
Boris Docker <madfure@gmail.com>
2024-08-17 00:25:02 +0300

    make secured connection to redis

--- bot/configs/env_reader.py
+++ bot/configs/env_reader.py
@@ -7,9 +7,11 @@ env_path = Path(".env")
 
 class Settings(BaseSettings):
     NESCAFEBOT_TOKEN: SecretStr
+    REDIS_PASSWORD: SecretStr
     model_config: SettingsConfigDict = SettingsConfigDict(
         env_file=env_path,
         env_file_encoding="utf-8"
     )
+    
 
 config = Settings()
