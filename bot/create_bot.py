e7703fc9662546c6161e207b8363285c579895f1
Vladislav Balabaev <vv.balabaev@gmail.com>
2024-09-01 14:03:47 +0800

    formatting

--- bot/create_bot.py
+++ bot/create_bot.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher
-
 from configs.env_reader import config
 
 
-bot = Bot(config.NESCAFEBOT_TOKEN.get_secret_value())
+bot = Bot(token=config.NESCAFEBOT_TOKEN.get_secret_value())
+
 dp = Dispatcher()
