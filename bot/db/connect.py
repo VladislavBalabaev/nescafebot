a1a14b0e899457639f023ef32af0e08b62005a08
Vladislav Balabaev <vv.balabaev@gmail.com>
2024-09-02 12:46:54 +0800

    test of mongo

--- bot/db/connect.py
+++ bot/db/connect.py
@@ -1,36 +1,46 @@
-import aioredis
 import asyncio
+from motor import motor_asyncio
 
 from configs.env_reader import config
 
 
-def connect_to_redis_users():
-    redis_con = aioredis.from_url(
-            url="redis://:" + config.REDIS_PASSWORD.get_secret_value() + "@redis_DB:6379", 
-            decode_responses=True,
-            db=1,
+
+
+def get_users_db():
+    client = motor_asyncio.AsyncIOMotorClient(
+        # f"mongodb://{config.MONGODB_USERNAME.get_secret_value()}:{config.MONGODB_PASSWORD.get_secret_value()}@mongo_DB:27017/?authMechanism=DEFAULT&directConnection=true"
+        f"mongodb://{config.MONGODB_USERNAME.get_secret_value()}:{config.MONGODB_PASSWORD.get_secret_value()}@localhost:27017/?authMechanism=DEFAULT&directConnection=true"
         )
 
-    return redis_con
+    db = client['userDatabase']
+    users = db['users']
+
+    return users
 
 
-def connect_to_redis_messages():
-    redis_con = aioredis.from_url(
-            url="redis://:" + config.REDIS_PASSWORD.get_secret_value() + "@redis_DB:6379", 
-            decode_responses=True,
-            db=2,
-        )
-
-    return redis_con
+mongo_users = get_users_db()
 
 
-def connect_to_pg_matching():
-    # TODO: create postgre db for matching
-    pg_con = ...
+async def insert_user_data(user_id, name, description, blacklist, whitelist, side, time, text):
+    global mongo_users
 
-    return pg_con
+    user_data = {
+        "_id": user_id,  # User ID as the key
+        "name": name,
+        "description": description,
+        "blacklist": blacklist,
+        "whitelist": whitelist,
+        "messages": {
+            "side": side,
+            "time": time,
+            "text": text
+        }
+    }
+    await mongo_users.insert_one(user_data)
 
 
-redis_users = connect_to_redis_users()
-redis_messages = connect_to_redis_messages()
-pg_matching = connect_to_pg_matching()
+async def get_user_data(user_id):
+    global mongo_users
+
+    user = await mongo_users.find_one({"_id": user_id})
+    return user
--- bot/db/structure.py
+++ bot/db/structure.py
@@ -1,113 +1,113 @@
-import json
-import operator
-from datetime import datetime
-from functools import reduce
+# import json
+# import operator
+# from datetime import datetime
+# from functools import reduce
 
-from db.connect import redis_users, redis_messages, pg_matching
-
-
-user_structure = {
-    "user_id": "",
-    "chat_id": "",
-    "info": {
-        "full_name": "",            # his name in tg
-        "username": "",             # his tg tag
-        "name": "",                 # how he calls himself in bot
-        "email": "",
-        "age": "",
-        "program": {
-            "year": "",
-            "name": "",
-            },
-        "about": "",
-    },
-    "messages": [],
-    "blacklist": {
-        "users": [],                # of user_ids 
-        "programs": []
-        },
-}
+# from db.connect import .
 
 
-def dict_get(data_dict, path):
-    """Access a nested object in data_dict by item sequence."""
-    return reduce(operator.getitem, path, data_dict)
+# user_structure = {
+#     "user_id": "",
+#     "chat_id": "",
+#     "info": {
+#         "full_name": "",            # his name in tg
+#         "username": "",             # his tg tag
+#         "name": "",                 # how he calls himself in bot
+#         "email": "",
+#         "age": "",
+#         "program": {
+#             "year": "",
+#             "name": "",
+#             },
+#         "about": "",
+#     },
+#     "messages": [],
+#     "blacklist": {
+#         "users": [],                # of user_ids 
+#         "programs": []
+#         },
+# }
 
 
-def dict_set(data_dict, path, value):
-    """Set a value in a nested object in data_dict by item sequence."""
-    dict_get(data_dict, path[:-1])[path[-1]] = value
+# def dict_get(data_dict, path):
+#     """Access a nested object in data_dict by item sequence."""
+#     return reduce(operator.getitem, path, data_dict)
 
 
-# def dict_del(data_dict, path):
-#     """Delete a key-value in a nested object in data_dict by item sequence."""
-#     del dict_get(data_dict, path[:-1])[path[-1]]
+# def dict_set(data_dict, path, value):
+#     """Set a value in a nested object in data_dict by item sequence."""
+#     dict_get(data_dict, path[:-1])[path[-1]] = value
 
 
-async def user_get(user_id, path = None):
-    data = await redis_users.get(str(user_id))
-    data = json.loads(data)
-
-    if path:
-        return dict_get(data, path)
-    else:
-        return data
+# # def dict_del(data_dict, path):
+# #     """Delete a key-value in a nested object in data_dict by item sequence."""
+# #     del dict_get(data_dict, path[:-1])[path[-1]]
 
 
-async def user_set(user_id, path: list = None, value: str = None):
-    if path:
-        data = await redis_users.get(str(user_id))
-        data = json.loads(data)
+# async def user_get(user_id, path = None):
+#     data = await redis_users.get(str(user_id))
+#     data = json.loads(data)
 
-        dict_set(data, path, str(value))
-    else:
-        data = user_structure
-
-    data = json.dumps(data)
-    await redis_users.set(str(user_id), data)
-
-    return
+#     if path:
+#         return dict_get(data, path)
+#     else:
+#         return data
 
 
-async def user_sent_msg(message, text: str = None):
-    messages = await redis_users.get(str(message.from_user.id))
-    print(messages)
-    messages = json.loads(messages)
+# async def user_set(user_id, path: list = None, value: str = None):
+#     if path:
+#         data = await redis_users.get(str(user_id))
+#         data = json.loads(data)
 
-    messages[str(message.from_user.id)].append({
-        "side": "bot",
-        "datetime": datetime.now(),
-        "message": text,
-    })
+#         dict_set(data, path, str(value))
+#     else:
+#         data = user_structure
 
+#     data = json.dumps(data)
+#     await redis_users.set(str(user_id), data)
 
-    messages = json.dumps(messages)
-    await user_set(str(message.from_user.id), ["messages"], messages)
-
-    await message.answer(text)
-
-    return
+#     return
 
 
-async def user_recieve_msg(message, text: str = None):
-    messages = await redis_users.get(str(message.from_user.id))
-    messages = json.loads(messages)
+# async def user_sent_msg(message, text: str = None):
+#     messages = await redis_users.get(str(message.from_user.id))
+#     print(messages)
+#     messages = json.loads(messages)
 
-    messages.append({
-        "side": "user",
-        "datetime": datetime.now(),
-        "message": text,
-    })
-
-    messages = json.dumps(messages)
-    await user_set(str(message.from_user.id), ["messages"], messages)
-
-    return
+#     messages[str(message.from_user.id)].append({
+#         "side": "bot",
+#         "datetime": datetime.now(),
+#         "message": text,
+#     })
 
 
-def save_redis_data():
-    ...
+#     messages = json.dumps(messages)
+#     await user_set(str(message.from_user.id), ["messages"], messages)
+
+#     await message.answer(text)
+
+#     return
 
 
-def program_to_users(program: str = "MAE'25"):
-    ...
+# async def user_recieve_msg(message, text: str = None):
+#     messages = await redis_users.get(str(message.from_user.id))
+#     messages = json.loads(messages)
+
+#     messages.append({
+#         "side": "user",
+#         "datetime": datetime.now(),
+#         "message": text,
+#     })
+
+#     messages = json.dumps(messages)
+#     await user_set(str(message.from_user.id), ["messages"], messages)
+
+#     return
+
+
+# def save_redis_data():
+#     ...
+
+
+# def program_to_users(program: str = "MAE'25"):
+#     ...
