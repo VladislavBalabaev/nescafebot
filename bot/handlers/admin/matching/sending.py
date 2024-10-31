4579fef991deaa098e5d2009a9250b239a6f56ff
Vladislav Balabaev <vv.balabaev@gmail.com>
2024-10-31 23:13:12 +0800

    keyboard row selection

--- bot/handlers/admin/matching/sending.py
+++ bot/handlers/admin/matching/sending.py
@@ -43,12 +43,12 @@ async def send_matching_client(matched: pd.DataFrame):
 
         row = matched.loc[matched["username"] == username].iloc[0]
 
-        return f"{row['info']['written_name']} (@{row['username']}) с {row['info']['program']['name']}'{row['info']['program']['year']}.\nСмайл пользователя - {row['emoji']}\nО себе: {row['info']['about']}"
+        return f"{row['info']['written_name']} (@{row['username']}) с {row['info']['program']['name']}'{row['info']['program']['year']}.\nСмайл пользователя - {row['emoji']}\n\nО себе: {row['info']['about']}"
 
 
     for user_id in matched.index:
         await send_msg_user(user_id, 
-                            f"Привет!\nПришли с результатами случайного кофе)\n\nТвой смайл - {matched.loc[user_id, 'emoji']}")
+                            f"Привет! Пришли с результатами\n\nТвой смайл - {matched.loc[user_id, 'emoji']}")
 
         assignments = matched.loc[user_id, 'assignments']
         n = len(assignments)
@@ -64,11 +64,8 @@ async def send_matching_client(matched: pd.DataFrame):
                                 info_by_username(assignments[0]))
 
             await send_msg_user(user_id, 
-                                "Ты можешь написать ему его смайл, он сразу поймет, что выпал тебе на кофе)")
+                                "Ты можешь написать ему его смайл. Он сразу поймет, что выпал тебе на кофе)")
         elif n == 2:
-            await send_msg_user(user_id,
-                                f"В этот раз тебе выпали двое человек, которым ты можешь написать:")
-
             await send_msg_user(user_id,
                                 info_by_username(assignments[0]))
 
@@ -76,12 +73,11 @@ async def send_matching_client(matched: pd.DataFrame):
                                 info_by_username(assignments[1]))
 
             await send_msg_user(user_id,
-                                f"Ты можешь написать им их смайл, они сразу поймут, что выпали тебе на кофе)")
+                                f"Ты можешь написать им их смайл. Они сразу поймут, что выпали тебе на кофе)")
         else:
             raise ValueError(f"_id='{user_id}' has more than 2 assignments.")
 
-        # TEMPORARY
         await send_msg_user(user_id,
-                            f"Хотим уточнить, что распределение участников асимметричное.\nТо есть, те люди, которые тебе выпадут, скорее всего не получат тебя")
+                            f"Напомним, что распределение участников асимметричное.\nТо есть, те люди, которые выпали тебе, скорее всего не получили тебя")
 
     return
--- bot/handlers/client/shared/keyboard.py
+++ bot/handlers/client/shared/keyboard.py
@@ -4,7 +4,15 @@ from aiogram import types
 def create_keyboard(choices):
     """
     Creates a reply keyboard with the provided choices. Each choice is displayed as a button.
-    The keyboard is resized for better display.
+    The keyboard is resized for better display. A maximum of 3 buttons is allowed per row.
     """
-    buttons = [[types.KeyboardButton(text=choice.value) for choice in choices]]
+    max_buttons_per_row = 3
+
+    choices_list = list(choices)
+
+    buttons = [
+        [types.KeyboardButton(text=choice.value) for choice in choices_list[i:i + max_buttons_per_row]]
+        for i in range(0, len(choices_list), max_buttons_per_row)
+    ]
+    
     return types.ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
